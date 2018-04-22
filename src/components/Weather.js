4a0844891d9608b958e9209a7fd80b0ab6d022ea
Justin Ndwiga <ndwigajustin@gmail.com>
2018-04-22 11:18:18 +0300

    Set states and display weather data in html

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,10 @@ export default class Weather extends Component {
     render() {
         return (
             <div>
-                Weather comp.
+                { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p> }
+                { this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
+                { this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
+                { this.props.description && <p>Conditions: {this.props.description}</p> }
             </div>
         );
     }
